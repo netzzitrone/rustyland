6e1dd85aedba6b5aa6a6dc29b4f7327ae68e148b
Daniel Bartschies <daniel.bartschies@blackbit.de>
2018-02-22 11:33:51 +0100

    add codefight solution: matrixElementsSum

--- codefights/edge_of_the_ocean/matrixElementsSum.rs
+++ codefights/edge_of_the_ocean/matrixElementsSum.rs
@@ -1,15 +1,32 @@
 fn matrixElementsSum(matrix: Vec<Vec<i32>>) -> i32 {
     let mut sum:i32 = 0;
 
+    let mut haunted = Vec::new();
+    
     for x in 0..matrix[0].len() {
         sum = sum + matrix[0][x];
+        if matrix[0][x] == 0 {
+            haunted.push(x);
+        }
     }
+    
     if matrix.len() > 1 {
         for y in 1..matrix.len() {
             for x in 0..matrix[y].len() {
-                //works unly with direct below empty rooms
+                let mut is_haunted = false;
+                for tmp in 0..haunted.len() {
+                    if haunted[tmp] == x {
+                        is_haunted = true;
+                        break;
+                    }
+                }
                 if matrix[y-1][x] > 0 {
-                    sum = sum + matrix[y][x];
+                    if is_haunted == false {
+                        sum = sum + matrix[y][x];
+                    }
+                }
+                else {
+                    haunted.push(x);
                 }
             }
         }
